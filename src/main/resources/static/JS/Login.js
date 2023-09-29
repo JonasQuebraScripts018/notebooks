105435a7862e0575286d8df46da7b6001e05d467
JonasQuebraScripts018 <gustavohpdelima@gmail.com>
2023-09-29 17:18:36 -0300

    isso ae tio

--- src/main/resources/static/JS/Login.js
+++ src/main/resources/static/JS/Login.js
@@ -1,5 +1,17 @@
 $("#login").click(enviarLogin);
 
+    $("#matricula").keyup(function(event){
+            if(event.key === "Enter"){
+               $("senha").focus();
+            }
+        });
+
+    $("#senha").keyup(function(event){
+        if(event.key === "Enter"){
+            enviarLogin();
+        }
+    });
+
     function enviarLogin(){
     let matricula = $("#matricula").val();
     let senha = $("#senha").val();
--- src/main/resources/static/JS/cadastroNote.js
+++ src/main/resources/static/JS/cadastroNote.js
@@ -1,21 +1,26 @@
 $("#cadastroNote").click(cadastrarNote);
+$("#sair").click(sair);
 
-function cadastrarNote(){
-    let numeroNote = $("#numero").val();
-    let patrimonio = $("#patrimonio").val();
+    function sair(){
+        window.location.href="Home/home";
+    }
 
-    $.ajax({
-        type: "POST",
-        url: "/cadastroNote",
-        data:{
-            numero:numeroNote,
-            patrimonio:patrimonio,
-        },
-        success:function(){
-            alert("ok");
-        },
-        error: function(){
-            alert("deu ruim lek, complicado");
-        }
-    });
-}
+    function cadastrarNote(){
+        let numeroNote = $("#numero").val();
+        let patrimonio = $("#patrimonio").val();
+
+        $.ajax({
+            type: "POST",
+            url: "/cadastroNote",
+            data:{
+                numero:numeroNote,
+                patrimonio:patrimonio,
+            },
+            success:function(data){
+                alert("ok");
+            },
+            error: function(){
+                alert("deu ruim lek, complicado");
+            }
+        });
+    }
